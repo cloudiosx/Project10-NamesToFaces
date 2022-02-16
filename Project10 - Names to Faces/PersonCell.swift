b54bb56a49a3f8d11c5d38c51ca956bba7b4b33d
John Kim <cloudiosx@gmail.com>
2022-02-16 18:28:23 -0500

    Designing UICollectionView cells

---
+++ Project10 - Names to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10 - Names to Faces
+//
+//  Created by John Kim on 2/16/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
+
---
+++ Project10 - Names to Faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10 - Names to Faces
+//
+//  Created by John Kim on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
